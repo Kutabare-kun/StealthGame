1c4d263a69bdd129c83fc544b405fe99c2b73687
Kutabare <78682049+Kutabare-kun@users.noreply.github.com>
2023-09-15 18:51:57 +0300

    Initial commit
    - First Person Template
    - Added  UI
    - Added Objective
    - SetUp Collision for Objective

---
+++ README.md
@@ -0,0 +1,2 @@
+# StealthGame
+ Udemy Course from Tom Looman
---
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSObjectiveActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class FPSGAME_API AFPSObjectiveActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AFPSObjectiveActor();
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	USphereComponent* SphereComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	UParticleSystem* PickupFX;
+	
+	virtual void BeginPlay() override;
+
+	void PlayEffects();
+
+public:
+	
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	
+};
